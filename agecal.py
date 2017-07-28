86473187080abc4be5f20916ac30596d419a27d8
Amol Office <info.amol@gmail.com>
2017-07-28 05:59:03 -0400

    Adding Random No code in python

--- agecal.py
+++ agecal.py
@@ -5,6 +5,6 @@ print('my Calc')
 number_1 = int(input('Enter first no :'))
 number_2 = int(input('Enter second no :'))
 
-print('{} +{} = '.format(number_1,number_2))
+print('{} + {} = '.format(number_1,number_2))
 print(number_1 + number_2)
 
---
+++ random_number_guess.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+print("Guess the Number between 1 to 100")
+random = 40
+#guess=int(input('Enter your guess' :))
+
+while(1==1 ):
+  guess=int(input('Enter your guess : '))
+  if guess == random: 
+     print('Bingo.. You are right.')
+     break
+  else:
+     print('Bad Luck.. Try againg')
+
