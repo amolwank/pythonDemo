b6355392d02bef9de4e4c2d976222734ade29746
Amol Office <info.amol@gmail.com>
2017-07-28 05:19:41 -0400

    Second python file

--- agecal.py
+++ agecal.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
 
-print('Hello World!')
+print('my Calc')
+
+number_1 = int(input('Enter first no :'))
+number_2 = int(input('Enter second no :'))
+
+print('{} +{} = '.format(number_1,number_2))
+print(number_1 + number_2)
 
